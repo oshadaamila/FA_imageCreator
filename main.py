fbc5c69863523a01489eb1f5587d8c3e3b5b1334
Oshada <oshadaamila.15@cse.mrt.ac.lk>
2022-06-03 10:01:32 +0530

    fixed images

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ from os.path import exists
 import sys
 import os
 from glob import glob
-from PIL import Image, ImageFont, ImageDraw 
+from PIL import Image, ImageFont, ImageDraw
+from tkinter import messagebox
 
 def find_indicator_position(water_level):
     if( water_level > 8.5) :
         return 410
-    elif (water_level < 0.4):
-        return 1120
+    elif (water_level < 1.0):
+        return 1060
     else:
         return 1200 - ((785/9)*water_level + 33)
 
@@ -26,7 +27,7 @@ def find_slider_position(water_level):
     if (water_level > 6.5) :
         return 410
     elif (water_level < 3.0):
-        return 900
+        return 800
     else:
         return find_indicator_position(water_level) - 160
 
@@ -51,13 +52,15 @@ def getAlertColor(alert):
         return (0,255,0)
     else:
         return (0,0,0)
-    
+
+
+# TODO implement
 
 r = requests.get('http://www.dmc.gov.lk/index.php?option=com_dmcreports&view=reports&Itemid=277&search=Water%20Level&report_type_id=6&todate=&fromdate=&lang=en')
- 
+
 # Parsing the HTML
 soup = BeautifulSoup(r.content, 'html.parser')
- 
+
 # Finding by id
 s = soup.find('td', class_= 'sandplustd')
 row_el = soup.find('tr', class_ = 'rowdate_0')
@@ -70,6 +73,7 @@ latestFileName = linkel.get('href').split('/')[3]
 
 if (exists(latestFileName)):
     print("latest File already exists!")
+    messagebox.showerror(title="Image Creator - Error", message="latest File already exists!")
     sys.exit(0)
 
 for file in glob('Water_Level*.pdf'):
@@ -95,11 +99,12 @@ with pdfplumber.open(latestFileName) as pdf:
     waterLevelStatus = str(table[2][11])
     message = "Flood Alert- Nagalagam Weediya \n" + firstTime + ": " + firstWaterLevel + "m\n" +secondTime + ": " + secondWaterLevel + "m\n" + "Water Level: " + waterLevelStatus
 
+
 #create Slider
 slider = Image.open("resources/template1.png")
 image_editable = ImageDraw.Draw(slider)
 image_editable.text((74,40), report_date, (0, 0, 0), font=ImageFont.truetype('resources/Roboto-Bold.ttf', 63))
-image_editable.text((70,98), formatTime(secondTime[15:len(secondTime)].upper()), (0, 0, 0), font=ImageFont.truetype('resources/Roboto-Bold.ttf', 43))
+image_editable.text((70,100), formatTime(secondTime[15:len(secondTime)].upper()), (0, 0, 0), font=ImageFont.truetype('resources/Roboto-Bold.ttf', 40))
 image_editable.text((74,175), secondWaterLevel + " ft", (0, 0, 0), font=ImageFont.truetype('resources/Roboto-Regular.ttf', 110))
 slider.save("slider.png")
 
@@ -116,8 +121,9 @@ sinhalaTxt = "kd.,.xùÈh "
 image_edit = ImageDraw.Draw(background)
 image_edit.text((630,int(slider_pos) - 80), sinhalaTxt, (0, 0, 0), font=ImageFont.truetype('resources/0KDSAMAN Regular.ttf', 70))
 image_edit.text(find_alert_position(float(secondWaterLevel)), waterLevelStatus.upper(), getAlertColor(waterLevelStatus.upper()),ImageFont.truetype('resources/Roboto-Bold.ttf', 80))
-background.save("finalImage.jpg")
+background.save("finalImage.png")
 
 
-
+messagebox.showinfo(title="Image Creator - Success", message="Image Created finalImage.jpg!")
+background.show()
 print (message)
